abf7ae3339d97eb421a8099ea3ebada665d1e236
Ganesh S <ganeshoddly@gmail.com>
2020-09-23 20:31:47 +0530

    add chatbot-server-api

---
+++ chatbot-server-api/src/main/java/io/swagger/model/Customer.java
@@ -0,0 +1,163 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.model.Category;
+import io.swagger.model.Tag;
+import io.swagger.model.Customer_old.StatusEnum;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.validation.annotation.Validated;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Customer_old
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-08-27T06:08:27.613Z[GMT]")
+
+@Entity
+@Table(name = "customer")
+public class Customer  {
+
+	@JsonProperty("id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id = null;
+
+	@JsonProperty("name")
+	private String name = null;
+
+	@JsonProperty("account")
+	private String account = null;
+
+	@JsonProperty("city")
+	private String city = null;
+
+	@JsonProperty("balance")
+	private Long balance = null;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Long getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Long balance) {
+		this.balance = balance;
+	}
+
+	public enum StatusEnum {
+		ACTIVE("active"),
+
+		PENDING("pending"),
+
+		INACTIVE("inactive"),
+
+		HOLD("sold");
+
+		private String value;
+
+		StatusEnum(String value) {
+			this.value = value;
+		}
+
+		@Override
+		@JsonValue
+		public String toString() {
+			return String.valueOf(value);
+		}
+
+		@JsonCreator
+		public static StatusEnum fromValue(String text) {
+			for (StatusEnum b : StatusEnum.values()) {
+				if (String.valueOf(b.value).equals(text)) {
+					return b;
+				}
+			}
+			return null;
+		}
+	}
+	@Enumerated(EnumType.STRING)
+	@JsonProperty("status")
+	private StatusEnum status = null;
+
+	public Customer status(StatusEnum status) {
+		this.status = status;
+		return this;
+	}
+
+	/**
+	 * pet status in the store
+	 * @return status
+	 **/
+	@ApiModelProperty(value = "pet status in the store")
+
+	public StatusEnum getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusEnum status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", account=" + account + ", city=" + city + ", balance="
+				+ balance + ", status=" + status + "]";
+	}
+
+
+	/*
+	 * @Override public String toString() { return "Customer [id=" + id + ", name="
+	 * + name + ", account=" + account + ", city=" + city + ", balance=" + balance +
+	 * "]"; }
+	 */
+	
+	
+
+}
